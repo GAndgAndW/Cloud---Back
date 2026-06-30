18356402335d02b098ec1c6bcc79c2051d485b6e
Ggw <gggw123456789999@163.com>
2026-06-30 16:28:36 +0800

    微服务项目首次提交

---
+++ src/main/java/springcloudali/SpringCloudAliApplication.java
@@ -0,0 +1,13 @@
+package springcloudali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springcloudali/SpringCloudAliApplicationTests.java
@@ -0,0 +1,13 @@
+package springcloudali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
